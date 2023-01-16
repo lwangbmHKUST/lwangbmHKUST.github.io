858981337005c084ffd420d2ecde26e055d5b80b
茶木 <chamu.wlp@alibaba-inc.com>
2023-01-16 14:00:28 +0800

    d

--- css/love.js
+++ css/love.js
@@ -174,7 +174,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("Click Here", 23, 10);
+            ctx.fillText("Start Here", 23, 10);
             ctx.restore();
         },
         clear: function() {
